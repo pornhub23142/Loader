fcc41a1831bba4777f38da5a205c9ce5b3bd5981
Super-hub-trash <110716968+pornhub23142@users.noreply.github.com>
2022-08-20 11:27:36 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,9 @@
 if game.PlaceId == 537413528 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/pornhub23142/lua_hack/main/Build%20A%20Boat%20For%20Treasure.sex"))()
+else
+    game.StarterGui:SetCore("SendNotification",{
+        Title = "Script";
+        Text = "the game was not support";
+    })
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/pornhub23142/lua_hack/main/Universal.lua"))()  
 end
