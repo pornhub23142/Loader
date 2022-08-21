47903c5fe4f9a28b38673bb9ace9b1bfe0249ea8
Super-hub-trash <110716968+pornhub23142@users.noreply.github.com>
2022-08-21 10:59:40 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,7 @@
 if game.PlaceId == 537413528 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/pornhub23142/lua_hack/main/Build%20A%20Boat%20For%20Treasure.sex"))()
+elseif game.PlaceId == 155615604 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/pornhub23142/lua_hack/main/Prison%20Life.lua"))()
 else
     game.StarterGui:SetCore("SendNotification",{
         Title = "Script";
