034848e8741aca9eaefbf80d3c60b37e9c20c3a4
Super-hub-trash <110716968+pornhub23142@users.noreply.github.com>
2022-09-09 18:44:33 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -9,6 +9,7 @@
 
 print("script was made by Florentino#6290")
 
+repeat wait() until game:IsLoaded()
 if game.PlaceId == 537413528 then
 	loadstring(game:HttpGet"https://raw.githubusercontent.com/pornhub23142/lua_hack/main/Build%20A%20Boat%20For%20Treasure.lua")()
 elseif game.PlaceId == 155615604 then
